c7172ec97ec784b7b632fccfb1e60506ac50c10a
Gadi <guppiefriedman@gmail.com>
2025-01-20 17:16:05 +0100

    Initialized maven support for project

---
+++ recipe-helper/src/main/java/com/chessemulator94/scraper/App.java
@@ -0,0 +1,13 @@
+package com.chessemulator94.scraper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
