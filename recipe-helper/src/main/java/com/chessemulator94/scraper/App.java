467e1345c9ac0b50eabca272034cbea4536ca315
Gadi <guppiefriedman@gmail.com>
2025-01-22 17:49:22 +0100

    Removed getDoc() and added basic instantiation of webpage object

--- recipe-helper/src/main/java/com/chessemulator94/scraper/App.java
+++ recipe-helper/src/main/java/com/chessemulator94/scraper/App.java
@@ -1,25 +1,32 @@
 package com.chessemulator94.scraper;
 
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+import org.jsoup.nodes.Element;;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        Document doc = getDoc("https://www.dirk.nl/");
-        System.out.println(doc.outerHtml());
+
+        Webpage dirkWebpage = new Webpage("https://www.dirk.nl/boodschappen");
+        dirkWebpage.setProductSiteMap("https://www.dirk.nl/products-sitemap.xml");
+
+        Document xmlDoc = Jsoup.parse(Scraper_Spoke.getDoc(dirkWebpage.getProductSiteMap()).html(), Parser.xmlParser());
+        
+        List<Element> allElements = xmlDoc.getElementsByTag("loc");
+
+        for (Element singleElement : allElements){
+            System.out.println(singleElement.text());
+            System.out.println();
+        }
+
+
+
     }
 
-    public static Document getDoc(String url) {
-        
-        Document doc = null; 
-        try {
-            doc = Jsoup.connect(url).get();
-        } catch (Exception e) {
-            System.out.println(e);
-        } 
-        return doc;
-    }
+
 }
