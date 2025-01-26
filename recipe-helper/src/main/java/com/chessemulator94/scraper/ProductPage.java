4b35c2739ed5e810d199d2951dec5fa3c7b6b22b
Gadi <guppiefriedman@gmail.com>
2025-01-26 12:23:43 +0100

    Redistributed funcationality between ProductPage and Webpage (ProductPage extends Webpage)

--- recipe-helper/src/main/java/com/chessemulator94/scraper/ProductPage.java
+++ recipe-helper/src/main/java/com/chessemulator94/scraper/ProductPage.java
@@ -1,9 +1,30 @@
 package com.chessemulator94.scraper;
 
+// ProductPage extends Webpage and stores website information related to all products of a webpage 
 public class ProductPage extends Webpage {
     
+    private String PRODUCTS_SITE_MAP;
+
     // Default constructor
     public ProductPage(){
+        this.setUrl("");
+        this.setSiteMap("");
+        this.setProductSiteMap("");
     }
+
+    // Constructor
+    public ProductPage(String URL, String SITE_MAP, String PRODUCTS_SITE_MAP){ 
+        this.setUrl(URL);
+        this.setSiteMap(SITE_MAP);
+        this.setProductSiteMap(PRODUCTS_SITE_MAP);
+    }
+
+    public void setProductSiteMap(String PRODUCTS_SITE_MAP){
+        this.PRODUCTS_SITE_MAP = PRODUCTS_SITE_MAP;
+    }
+
+    public String getProductSiteMap(){
+        return this.PRODUCTS_SITE_MAP;
+    } 
 
 }
--- recipe-helper/src/main/java/com/chessemulator94/scraper/Webpage.java
+++ recipe-helper/src/main/java/com/chessemulator94/scraper/Webpage.java
@@ -1,56 +1,61 @@
 package com.chessemulator94.scraper;
 
 import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 
 public class Webpage {
     
     // Parameters
         // e.g can be found by googling "site:dirk.nl filetype:xml"
-    private String BASE_SITE_MAP;
-    private String PRODUCTS_SITE_MAP;
     private String URL;
+    private String SITE_MAP;
     private List<Element> domElements;
+    private Document XML_STRUCTURE;
 
     // Default Constructor
     public Webpage(){
-        this.BASE_SITE_MAP = "";
-        this.PRODUCTS_SITE_MAP = "";
+        this.SITE_MAP = "";
         this.URL = "";          
     }
 
     // Constructor
     public Webpage(String URL){
         this.URL = URL;
-        this.BASE_SITE_MAP = "";
-        this.PRODUCTS_SITE_MAP = "";
+        this.SITE_MAP = "";
     }
 
-    // Constructor   
-    public Webpage(String URL, String BASE_SITE_MAP, String PRODUCTS_SITE_MAP){ 
+    // Constructor with sitemap  
+    public Webpage(String URL, String SITE_MAP){ 
         this.URL = URL;
-        this.BASE_SITE_MAP = BASE_SITE_MAP;
-        this.PRODUCTS_SITE_MAP = PRODUCTS_SITE_MAP;
+        this.SITE_MAP = SITE_MAP;
     }
 
     // Getters and Setters
-    public void setBaseSiteMap(String BASE_SITE_MAP){
-        this.BASE_SITE_MAP = BASE_SITE_MAP;
-    }
-    public void setProductSiteMap(String PRODUCTS_SITE_MAP){
-        this.PRODUCTS_SITE_MAP = PRODUCTS_SITE_MAP;
+    public void setSiteMap(String SITE_MAP){
+        this.SITE_MAP = SITE_MAP;
     }
     public void setUrl(String URL){
         this.URL = URL;
-    }
-    public String getBaseSiteMap(){
-        return BASE_SITE_MAP;
+        }
+    public String getSiteMap(){
+        return SITE_MAP;
     }      
-    public String getProductSiteMap(){
-        return PRODUCTS_SITE_MAP;
-    }   
     public String getUrl(){
         return URL;
-    }   
+    }  
+    
+    //
+    public void generateXmlStructure(){
+        this.XML_STRUCTURE = Jsoup.parse(Scraper.getDoc(this.getSiteMap()).html(), Parser.xmlParser());
+    }
+
+    //
+    public void generateDomTree(){
+
+    }
 
 }
